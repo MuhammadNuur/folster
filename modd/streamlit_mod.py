efafb77ec18d7017ae8d46eeb28b30f872ddbc6f
MuhammadNuur <muh.nuur@gmail.com>
2024-08-14 15:20:57 +0900

    second update

--- modd/streamlit_mod.py
+++ modd/streamlit_mod.py
@@ -87,11 +87,11 @@ def goes_wild():
         st.color_picker("Pick a color")
 
         # Use widgets' returned values in variables:
-        for i in range(int(st.number_input("Num:"))):
-            foo()
-        if st.sidebar.selectbox("I:",["f"]) == "f":
-            b()
-        my_slider_val = st.slider("Quinn Mallory", 1, 88)
+        # for i in range(int(st.number_input("Num:"))):
+        #     foo()
+        # if st.sidebar.selectbox("I:",["f"]) == "f":
+        #     b()
+        slider_val = st.slider("Quinn Mallory", 1, 88)
         st.write(slider_val)
 
         # Disable widgets to remove interactivity:
