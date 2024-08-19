47c77774d421d20bb1cd7cfd9c1c2f4abeaca20b
MuhammadNuur <muh.nuur@gmail.com>
2024-08-19 11:23:33 +0900

    Fix Map Size 2

--- modd/streamlit_mod.py
+++ modd/streamlit_mod.py
@@ -26,6 +26,7 @@ def goes_wild():
     hide_menu_style = """
             <style>
             #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
             </style>
             """
     st.markdown(hide_menu_style, unsafe_allow_html=True)
